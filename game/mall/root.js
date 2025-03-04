931f237a16a55de765d31b1eb1642d09b6fb59e1
Michal Mihálik <michal.mhlk@gmail.com>
2025-03-04 11:44:46 +0100

    hashing

--- game/mall/root.js
+++ game/mall/root.js
@@ -1,7 +1,7 @@
 
 window.linkRes = (url) => {
     if (window.integrationSite) {
-        return "https://cdn.jsdelivr.net/gh/joyinmall/joyinmall.github.io@62a2355/game/" + url;
+        return "https://cdn.jsdelivr.net/gh/joyinmall/joyinmall.github.io@63fb80a/game/" + url;
     } else {
         return url;
     }
