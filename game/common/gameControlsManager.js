6c1ba3270c465e0b4aa793c43749c30a4c5a8d02
Michal Mihálik <michal.mhlk@gmail.com>
2025-03-04 13:55:06 +0100

    pointer fix

--- game/common/gameControlsManager.js
+++ game/common/gameControlsManager.js
@@ -93,6 +93,7 @@ class GameControlsManager {
     hide() {
         this.introImage.isVisible = false;
         this.controlDialog.isVisible = false;
+        document.body.style.cursor = "default";
     }
 
     addButtons(row) {
